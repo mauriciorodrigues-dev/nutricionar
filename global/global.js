99ecbb9e4883f5f484a4df274be6555a49d14b0a
Mauricio Rodrigues <143841940+mauriciorodrigues-dev@users.noreply.github.com>
2024-02-15 19:30:10 -0300

    2

--- global/global.js
+++ global/global.js
@@ -1,3 +1,5 @@
+
+//Função que cumprimenta o atleta conforme o horário
 document.addEventListener("DOMContentLoaded", function() {
   saudacao();
 });
@@ -17,4 +19,49 @@ function saudacao() {
     saud.innerHTML = "Bom Dia, Mauricio!";
   }
 }
+
+
+//função que comanda a troca de abas dentro da página do atleta
+function ativarLinkDaSecaoAtiva() {
+  // Pega a seção ativa
+  var secaoAtiva = document.querySelector('.secao.ativo');
+
+  // Remove a classe 'ativo' de todos os links na sidebar
+  document.querySelectorAll('.sidebar a').forEach(link => {
+    link.classList.remove('ativo');
+  });
+
+  // Adiciona a classe 'ativo' ao link correspondente na sidebar, se a seção estiver ativa
+  if (secaoAtiva) {
+    var secaoId = secaoAtiva.id;
+    var linkAtivo = document.querySelector('.sidebar a[href="#' + secaoId + '"]');
+    if (linkAtivo) {
+      linkAtivo.classList.add('ativo');
+    }
+  }
+}
+
+
+function mostrarSecao(secao) {
+  // Oculta todas as seções
+  document.querySelectorAll('.secao').forEach(secaoItem => {
+    secaoItem.style.display = 'none';
+    secaoItem.classList.remove('ativo');
+  });
+
+  // Mostra apenas a seção desejada
+  var secaoSelecionada = document.getElementById(secao);
+  secaoSelecionada.style.display = 'block';
+  secaoSelecionada.classList.add('ativo');
+
+  // Ativa o link correspondente na sidebar
+  ativarLinkDaSecaoAtiva();
+}
+
+// Inicialmente mostra a seção 'home'
+mostrarSecao('home');
+
+
+
+
 
