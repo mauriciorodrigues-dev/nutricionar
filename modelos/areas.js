fb21a0743a700189569c04cb0c42655e23a87524
Mauricio Rodrigues <143841940+mauriciorodrigues-dev@users.noreply.github.com>
2024-02-16 00:22:48 -0300

    update charts

---
+++ modelos/areas.js
@@ -0,0 +1,65 @@
+// Gráfico de evolução de peso
+// Seus dados de peso corporal (substitua pelos seus próprios dados)
+var dadosPeso = [65, 67, 68, 70, 72, 71, 73, 79];
+
+// Configurar o contexto do gráfico
+var ctx = document.getElementById("grafico-peso").getContext("2d");
+
+// Criar o gráfico de linha
+var graficoPeso = new Chart(ctx, {
+  type: "line",
+  data: {
+    labels: [
+      "Semana 1",
+      "Semana 2",
+      "Semana 3",
+      "Semana 4",
+      "Semana 5",
+      "Semana 6",
+      "Semana 7",
+      "Semana 8",
+    ],
+    datasets: [
+      {
+        label: "Peso na Semana",
+        data: dadosPeso,
+        borderColor: "rgba(75, 192, 192, 1)",
+        borderWidth: 2,
+        fill: false,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: false,
+      },
+    },
+  },
+});
+
+
+const graficoMacros = {
+  labels: ['Gordura', 'Proteína', 'Carbo'],
+  datasets: [{
+    label: ' Quantidade (%)',
+    data: [20, 30, 50],
+    backgroundColor: [
+      'rgb(255, 99, 132)',
+      'rgb(54, 162, 235)',
+      'rgb(255, 205, 86)'
+    ],
+    hoverOffset: 4
+  }]
+};
+
+let ctc2 = document.getElementById("grafico-macros").getContext("2d");
+
+const config = {
+  type: 'doughnut',
+  data: graficoMacros,
+};
+
+const grafico = new Chart(ctc2, config);
+
+
