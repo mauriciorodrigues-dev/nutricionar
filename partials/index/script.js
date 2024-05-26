1728c312754365d3d384f33f75c00d27549fdf2c
Mauricio Rodrigues <143841940+mauriciorodrigues-dev@users.noreply.github.com>
2024-05-25 23:16:45 -0300

    a

---
+++ partials/index/script.js
@@ -0,0 +1,9 @@
+const loginButton = document.getElementById('login');
+const loginContainer = document.querySelector('.login-container');
+
+loginButton.addEventListener('click', function() {
+  loginContainer.classList.add('show');
+});
+
+// Como criar um toggle com essa classe show?
+
