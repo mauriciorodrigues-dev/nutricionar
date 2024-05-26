9addff8e5d3f414e421813c060b243d2f13338dd
Mauricio Rodrigues <143841940+mauriciorodrigues-dev@users.noreply.github.com>
2024-05-26 09:36:13 -0300

    salvar

--- partials/index/script.js
+++ partials/index/script.js
@@ -7,3 +7,9 @@ loginButton.addEventListener('click', function() {
 
 // Como criar um toggle com essa classe show?
 
+
+// como acessar uma página do projeto atraves de um botão?
+
+document.querySelector('#logar').addEventListener('click', function() {
+  window.location.href = '../../atleta/modelo.html';
+});
