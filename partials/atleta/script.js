42b33d7d4bcfb2da201ddacda79287a215ccdb08
Mauricio Rodrigues <143841940+mauriciorodrigues-dev@users.noreply.github.com>
2024-06-01 23:31:05 -0300

    au

--- partials/atleta/script.js
+++ partials/atleta/script.js
@@ -1,27 +1,29 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//   const buttons = {
-//       btnDashboard: 'atleta__dashboard',
-//       btnTreino: 'atleta__treino',
-//       btnDieta: 'atleta__dieta',
-//       btnMedidas: 'atleta__medidas'
-//       btnHome: 'atleta__medidas'
-//   };
+document.addEventListener("DOMContentLoaded", function () {
+  const buttons = {
+    btnDashboard: "atleta__dashboard",
+    btnTreino: "atleta__treino",
+    btnDieta: "atleta__dieta",
+    btnMedidas: "atleta__medidas",
+    btnHome: "atleta__medidas",
+  };
 
-//   Object.keys(buttons).forEach(buttonId => {
-//       document.getElementById(buttonId).addEventListener('click', function() {
-//           // Remove active class from all sections
-//           document.querySelectorAll('.section').forEach(section => section.classList.remove('active'));
+  Object.keys(buttons).forEach((buttonId) => {
+    document.getElementById(buttonId).addEventListener("click", function () {
+      // Remove active class from all sections
+      document
+        .querySelectorAll(".section")
+        .forEach((section) => section.classList.remove("active"));
 
-//           // Add active class to the selected section
-//           document.getElementById(buttons[buttonId]).classList.add('active');
-//       });
-//   });
-// });
+      // Add active class to the selected section
+      document.getElementById(buttons[buttonId]).classList.add("active");
+    });
+  });
+});
 
-const faqs = document.querySelectorAll('.faq')
+const faqs = document.querySelectorAll(".faq");
 
-faqs.forEach(faq => {
-  faq.addEventListener('click', ()=>{
-    faq.classList.toggle('active');
-  })
-  })
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("active");
+  });
+});
