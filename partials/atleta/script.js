bda00ca4a1d73aa68b8c0b0633a2c81836e64bfa
Mauricio Rodrigues <143841940+mauriciorodrigues-dev@users.noreply.github.com>
2024-06-03 11:45:52 -0300

    u

--- partials/atleta/script.js
+++ partials/atleta/script.js
@@ -27,3 +27,60 @@ faqs.forEach((faq) => {
     faq.classList.toggle("active");
   });
 });
+
+//Carrousel
+document.addEventListener("DOMContentLoaded", () => {
+  let currentIndex = 0;
+  const slides = document.querySelector(".gallery");
+  const totalSlides = slides.children.length;
+  const visibleSlides = totalSlides / 2; // Porque duplicamos
+
+  function showSlide(index) {
+    if (index >= totalSlides - visibleSlides) {
+      slides.style.transition = "none";
+      currentIndex = index - visibleSlides;
+      slides.style.transform = `translateX(${
+        (-currentIndex * 100) / visibleSlides
+      }%)`;
+      setTimeout(() => {
+        slides.style.transition = "transform 0.5s ease-in-out";
+        currentIndex++;
+        slides.style.transform = `translateX(${
+          (-currentIndex * 100) / visibleSlides
+        }%)`;
+      }, 50);
+    } else if (index < 0) {
+      slides.style.transition = "none";
+      currentIndex = totalSlides - visibleSlides;
+      slides.style.transform = `translateX(${
+        (-currentIndex * 100) / visibleSlides
+      }%)`;
+      setTimeout(() => {
+        slides.style.transition = "transform 0.5s ease-in-out";
+        currentIndex--;
+        slides.style.transform = `translateX(${
+          (-currentIndex * 100) / visibleSlides
+        }%)`;
+      }, 50);
+    } else {
+      currentIndex = index;
+      slides.style.transform = `translateX(${
+        (-currentIndex * 100) / visibleSlides
+      }%)`;
+    }
+  }
+
+  function nextSlide() {
+    showSlide(currentIndex + 1);
+  }
+
+  function prevSlide() {
+    showSlide(currentIndex - 1);
+  }
+
+  document.querySelector(".btn-next").addEventListener("click", nextSlide);
+  document.querySelector(".btn-prev").addEventListener("click", prevSlide);
+
+  // Iniciar carrossel no índice zero
+  showSlide(currentIndex);
+});
