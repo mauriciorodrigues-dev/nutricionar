e4f94f9d73658d7f53f65e4336dd2c59cec84da4
Mauricio Rodrigues <143841940+mauriciorodrigues-dev@users.noreply.github.com>
2024-06-07 16:13:57 -0300

    Update script.js

--- partials/atleta/script.js
+++ partials/atleta/script.js
@@ -85,7 +85,7 @@ document.addEventListener("DOMContentLoaded", function () {
 // });
 
 //Carrousel
-let slideIndex = 1;
+let slideIndex = 0;
 showSlides(slideIndex);
 
 // Next/previous controls
@@ -113,3 +113,19 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 } 
+
+
+
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
